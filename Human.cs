8a1cc7fa51680b093c396b73ae35cef9bdb0a98c
h-latta <h.latta@yahoo.com>
2022-03-25 16:06:12 -0500

    Created Human Class

---
+++ Human.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS_C
+{
+    public class Human : Player
+    {
+        //member variables
+
+        //constructor
+        public Human()
+        {
+
+        }
+        //member methods
+    }
+}
