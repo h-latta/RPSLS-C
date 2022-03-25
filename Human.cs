842c1c1bd8fc1bf07d3706ad75ae31619f075666
h-latta <h.latta@yahoo.com>
2022-03-25 16:46:04 -0500

    Created Game Intro in Program File

--- Human.cs
+++ Human.cs
@@ -16,5 +16,7 @@ namespace RPSLS_C
 
         }
         //member methods
+
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,13 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
 
 namespace Foundations
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Welcome to RPSLS!");
+            Console.WriteLine(" ");
+            Console.WriteLine("Rock crushes Scissors");
+            Console.WriteLine("Scissors cuts Paper");
+            Console.WriteLine("Paper covers Rock");
+            Console.WriteLine("Rock crushes Lizard");
+            Console.WriteLine("Lizard poisons Spock");
+            Console.WriteLine("Spock smashes Scissors");
+            Console.WriteLine("Scissors decapitates Lizard");
+            Console.WriteLine("Lizard eats Paper");
+            Console.WriteLine("Paper disproves Spock");
+            Console.WriteLine("Spock vaporizes Rock");
+            Console.ReadLine();
         }
     }
 }
